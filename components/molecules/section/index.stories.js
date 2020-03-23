18a744f5cad0fbd76af2be0150aeb1217df07e99
ysd-marrrr <41146529+ysd-marrrr@users.noreply.github.com>
2020-03-24 01:29:51 +0900

    SCSSを使えるようにした

--- components/molecules/section/index.stories.js
+++ components/molecules/section/index.stories.js
@@ -1,5 +1,11 @@
 import { storiesOf } from '@storybook/vue'
+import Introduction from '@/components/molecules/section/Introduction.vue'
 import Skill from '@/components/molecules/section/Skill.vue'
+
+storiesOf('/molecules/section/Introduction', module).add('default', () => ({
+  components: { Introduction },
+  template: '<introduction />'
+}))
 
 storiesOf('/molecules/section/Skill', module).add('default', () => ({
   components: { Skill },
