34ab78d05fa75015707043bca12cd02750f4fbb8
ysd-marrrr <41146529+ysd-marrrr@users.noreply.github.com>
2020-04-01 23:28:10 +0900

    PROJECT: プロジェクトカードを仮作製

---
+++ components/molecules/Card/ProjectCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="project-card">
+    <div class="project-header">
+      <p class="project-date">2019</p>
+    </div>
+    <div class="project-image no-image image is-square">
+      PRINTING
+    </div>
+    <div class="project-content">
+      <h3>PROJECT TITLE</h3>
+      <p class="project-description">description</p>
+    </div>
+    <div class="project-footer">
+      TAG
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.project-card {
+  background: white;
+  border-radius: 5px;
+  box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.5);
+  padding: 5px;
+}
+
+.project-date {
+  font-weight: bold;
+  font-size: 1.25rem;
+  color: gray;
+}
+
+.project-image {
+  width: 75%;
+  margin: auto;
+  background: gray;
+  color: white;
+  text-align: center;
+}
+</style>
--- components/molecules/Card/index.stories.js
+++ components/molecules/Card/index.stories.js
@@ -1,7 +1,13 @@
 import { storiesOf } from '@storybook/vue'
 import SkillCard from '@/components/molecules/Card/SkillCard.vue'
+import ProjectCard from '@/components/molecules/Card/ProjectCard.vue'
 
 storiesOf('/molecules/Card/SkillCard', module).add('default', () => ({
   components: { SkillCard },
   template: '<skill-card />'
+}))
+
+storiesOf('/molecules/Card/ProjectCard', module).add('default', () => ({
+  components: { ProjectCard },
+  template: '<project-card />'
 }))
