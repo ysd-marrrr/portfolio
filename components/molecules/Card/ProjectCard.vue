b0500370d59c3a2f22d1aae23d42117b9792d96c
ysd-marrrr <41146529+ysd-marrrr@users.noreply.github.com>
2020-04-03 23:46:12 +0900

    PROJECT: カードをレスポンシブにした

--- components/molecules/Card/ProjectCard.vue
+++ components/molecules/Card/ProjectCard.vue
@@ -3,8 +3,8 @@
     <div class="project-header">
       <p class="project-date">2019</p>
     </div>
-    <div class="project-image no-image image is-square">
-      PRINTING
+    <div class="project-image no-image">
+      <div class="inner">PRINTING</div>
     </div>
     <div class="project-content">
       <h3 class="project-title">PROJECT TITLE</h3>
@@ -30,11 +30,69 @@
   color: gray;
 }
 
+// 正方形のまま「レスポンシブに」サイズを小さくする
+// thx: http://wreath-ent.co.jp/blog-3560/
 .project-image {
-  margin: auto 15%;
+  position: relative;
+  width: 85%;
+  height: auto;
+  text-align: center;
+
+  &:before {
+    content: '';
+    display: block;
+    padding-top: 100%;
+  }
+
+  .inner {
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 100%;
+  }
+}
+
+// タブレット以上の場合は横に複数枚並べる
+@media screen and (min-width: 769px) {
+  .project-card {
+    max-width: 20rem;
+  }
+
+  .project-image {
+    width: 95%;
+
+    .inner {
+      left: 2.5%;
+    }
+  }
+}
+
+// モバイルの場合は横いっぱいカードを表示する
+@media screen and (max-width: 768px) {
+  .project-card {
+    max-width: 100vw;
+  }
+
+  .project-image {
+    width: 100%;
+    .inner {
+      left: 0;
+    }
+  }
+}
+
+.adjust-box p {
+  line-height: 1.5em;
+}
+
+.inner {
+  position: absolute;
   background: gray;
   color: white;
-  text-align: center;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
 
 h3.project-title {
