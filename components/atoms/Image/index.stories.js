a1def752575cadd28760a86b3232b07169252680
ysd-marrrr <41146529+ysd-marrrr@users.noreply.github.com>
2020-04-06 23:33:20 +0900

    PROJECT: プロジェクト画像のありなしに分けてstoryを追加した

--- components/atoms/Image/index.stories.js
+++ components/atoms/Image/index.stories.js
@@ -2,12 +2,24 @@ import { storiesOf } from '@storybook/vue'
 import ProjectImage from '@/components/atoms/Image/ProjectImage.vue'
 import MiniProjectImage from '@/components/atoms/Image/MiniProjectImage.vue'
 
-storiesOf('/atoms/Image/ProjectImage', module).add('default', () => ({
-  components: { ProjectImage },
-  template: '<project-image />'
-}))
+storiesOf('/atoms/Image/ProjectImage', module)
+  .add(':has-image', () => ({
+    components: { ProjectImage },
+    template:
+      '<project-image :image-url-prop="\'https://placebear.com/640/480\'"/>'
+  }))
+  .add(':no-image', () => ({
+    components: { ProjectImage },
+    template: '<project-image />'
+  }))
 
-storiesOf('/atoms/Image/MiniProjectImage', module).add('default', () => ({
-  components: { MiniProjectImage },
-  template: '<mini-project-image />'
-}))
+storiesOf('/atoms/Image/MiniProjectImage', module)
+  .add(':has-image', () => ({
+    components: { MiniProjectImage },
+    template:
+      '<mini-project-image :image-url-prop="\'https://placebear.com/640/480\'"/>'
+  }))
+  .add(':no-image', () => ({
+    components: { MiniProjectImage },
+    template: '<mini-project-image />'
+  }))
