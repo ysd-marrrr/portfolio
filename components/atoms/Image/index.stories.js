f65e75f4fa3168a13186c2ce43da109ca49b4875
ysd-marrrr <41146529+ysd-marrrr@users.noreply.github.com>
2020-04-06 23:29:28 +0900

    PROJECT: 小さいプロジェクト画像を作った

--- components/atoms/Image/index.stories.js
+++ components/atoms/Image/index.stories.js
@@ -1,7 +1,13 @@
 import { storiesOf } from '@storybook/vue'
 import ProjectImage from '@/components/atoms/Image/ProjectImage.vue'
+import MiniProjectImage from '@/components/atoms/Image/MiniProjectImage.vue'
 
 storiesOf('/atoms/Image/ProjectImage', module).add('default', () => ({
   components: { ProjectImage },
   template: '<project-image />'
+}))
+
+storiesOf('/atoms/Image/MiniProjectImage', module).add('default', () => ({
+  components: { MiniProjectImage },
+  template: '<mini-project-image />'
 }))
